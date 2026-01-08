d0a3570b289451d7ed300da0799a9ecbb071c174
AnrewKraund <gfgf05042003@gmail.com>
2026-01-08 14:44:54 +0500

    dd

--- sddm-theme.nix
+++ sddm-theme.nix
@@ -1,17 +1,14 @@
-{ stdenv}:
-{
-  stdenv.mkDerivation rec {
-    pname = "sddm-astronaut-theme";
-    version = "1";
-    
-    # Укажите src относительно файла
-    src = ./config/sddm/sddm-astronaut-theme;
-    
-    dontBuild = true;
-    
-    installPhase = ''
-      mkdir -p $out/share/sddm/themes
-      cp -aR $src $out/share/sddm/themes/sddm-astronaut-theme
-    '';
-  };
+{ stdenv }:
+stdenv.mkDerivation rec {
+  pname = "sddm-astronaut-theme";
+  version = "1";
+  
+  src = ./config/sddm/sddm-astronaut-theme;
+  
+  dontBuild = true;
+  
+  installPhase = ''
+    mkdir -p $out/share/sddm/themes
+    cp -aR $src $out/share/sddm/themes/sddm-astronaut-theme
+  '';
 }
